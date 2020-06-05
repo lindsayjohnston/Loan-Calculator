dc6858c5a382e084bd4541a1d3626fc1b07971bd
Lindsay Johnston <lindsaykjohnston@outlook.com>
2020-06-05 13:15:29 -0700

    Added spinner GIF to show  "during calculation", hide calculation until it's time to show it

--- docs/app.js
+++ docs/app.js
@@ -1,6 +1,14 @@
-document.getElementById('loan-form').addEventListener('submit', calculateResults);
+document.getElementById('loan-form').addEventListener('submit', function(e){
+    document.getElementById('results').style.display= 'none';
 
-function calculateResults(e){
+    document.getElementById('loading').style.display= 'block';
+
+    setTimeout(calculateResults, 2000);
+
+    e.preventDefault();
+});
+
+function calculateResults(){
     console.log("calculation");
     
     const amount= document.getElementById('amount');
@@ -22,14 +30,18 @@ function calculateResults(e){
         monthlyPayment.value= monthly.toFixed(2);
         totalPayment.value= (monthly * calculatedPayments).toFixed(2);
         totalInterest.value= ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display= 'block';
+        document.getElementById('loading').style.display= 'none';
     } else {
         showError('Please check your numbers')
     }
 
-    e.preventDefault();
 }
 
 function showError(error){
+    document.getElementById('results').style.display= 'none';
+    document.getElementById('loading').style.display= 'none';
+    
     const errorDiv= document.createElement('div');
 
     const card= document.querySelector('.card');
